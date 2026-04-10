94dd8e4bd846b494ef48038d93cb7b93fea22451
Reinier Hernández <sasuke.reinier@gmail.com>
2026-04-10 16:28:58 -0300

    feat(casks): add chimedock cask and extend verification workflow

---
+++ Casks/chimedock.rb
@@ -0,0 +1,11 @@
+cask "chimedock" do
+  version "1.1.0"
+  sha256 "2f0a3984bd8742efbaeea9a7a7d0afd10fb00290c93403c3798a7f69711a664c"
+
+  url "https://github.com/ragnarok22/ChimeDock/releases/download/v#{version}/ChimeDock.dmg"
+  name "ChimeDock"
+  desc "macOS menu bar app that plays chime sounds for USB device events"
+  homepage "https://github.com/ragnarok22/ChimeDock"
+
+  app "ChimeDock.app"
+end
